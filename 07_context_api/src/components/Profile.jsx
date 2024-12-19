75e0a8547e06fa982f23cfb734bbaf5cc48d6c0c
Nakshatra Vyas <nakshatravyas2004@gmail.com>
2024-12-19 23:34:05 +0530

    Context API

---
+++ 07_context_api/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Profile() {
+    const {user}=useContext(UserContext)
+  return (
+    <>
+            {!user ? (
+                <h2>Please login</h2>
+            ) : (
+                <h2>Welcome {user.username}</h2>
+            )}
+    </>
+  )
+}
+
+export default Profile
